8ffb5a24e0466c969714c8c689953aaae44d1b3d
LaBlaugrana <aayush.dubey.21cse@bmu.edu.in>
2023-06-27 10:57:25 +0530

    hihi

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/screens/splash_onboard/splash_onbo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
